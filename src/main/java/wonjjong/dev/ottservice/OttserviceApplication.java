2fcc144859ba8431ffec460eb7a4f803718143df
wonjjong <wonjjong.dev@gmail.com>
2022-02-19 07:52:05 +0900

    Initial commit

---
+++ src/main/java/wonjjong/dev/ottservice/OttserviceApplication.java
@@ -0,0 +1,13 @@
+package wonjjong.dev.ottservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OttserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OttserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wonjjong/dev/ottservice/OttserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package wonjjong.dev.ottservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OttserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
