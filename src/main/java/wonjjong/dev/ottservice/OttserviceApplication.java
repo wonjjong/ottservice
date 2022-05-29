24cd87201c091fde32224f20e2cc8afa86b4630b
wonjjong <wonjjong.dev@gmail.com>
2022-05-29 13:47:16 +0900

    h2 설치 및 jpa BaseTimeEntity 추가

--- src/main/java/wonjjong/dev/ottservice/OttserviceApplication.java
+++ src/main/java/wonjjong/dev/ottservice/OttserviceApplication.java
@@ -2,7 +2,9 @@ package wonjjong.dev.ottservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class OttserviceApplication {
 
